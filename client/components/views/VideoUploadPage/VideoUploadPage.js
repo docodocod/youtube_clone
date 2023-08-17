cef57548a626b963f98099ed5159800bf75a45e0
docodocod <dongan0618@naver.com>
2023-08-17 22:39:15 +0900

    component_create

---
+++ client/components/views/VideoUploadPage/VideoUploadPage.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {Typography, Button, Form, message, Input,Icon} from 'antd';
+
+const {Title}=Typography;
+
+function VideoUploadPage(){
+    return (
+        <div style={{maxWidth:'700px', margin:'2rem auto'}}>
+            <div style={{textAlign:'center', marginBottom:'2rem'}}>
+                <Title level={2}>Upload Video</Title>
+            </div>
+
+            <Form>
+                <div> style</div>
+            </Form>
+    )
+}
+
+export default VideoUploadPage
